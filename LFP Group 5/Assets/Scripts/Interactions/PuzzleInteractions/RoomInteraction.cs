affefbd59fa15dfd39ee7688ee388eff2d321de6
Phemelo Tsogang <2543064@students.wits.ac.za>
2026-09-21 15:56:07 +0200

    Room Re-entry Updated and fixed

--- LFP Group 5/Assets/Scripts/Interactions/PuzzleInteractions/RoomInteraction.cs
+++ LFP Group 5/Assets/Scripts/Interactions/PuzzleInteractions/RoomInteraction.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class RoomInteraction : MonoBehaviour
 {
     [SerializeField] private GameObject _trigger;
+    private Collider2D _collider;
 
     private void Awake()
     {
+        _collider = GetComponent<Collider2D>();
         _trigger.SetActive(true); ;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _trigger.SetActive(false);
+        _collider.enabled = false;
+
     }
 }
