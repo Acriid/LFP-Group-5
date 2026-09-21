aa563586d9a259a6d926a8a0bce3eaa802ca8512
Phemelo Tsogang <2543064@students.wits.ac.za>
2026-09-21 15:50:04 +0200

    Room Enter And Exit Fix

--- LFP Group 5/Assets/Scripts/Interactions/PuzzleInteractions/EnableVirus.cs
+++ LFP Group 5/Assets/Scripts/Interactions/PuzzleInteractions/EnableVirus.cs
@@ -3,15 +3,19 @@ using UnityEngine;
 public class EnableVirus : MonoBehaviour
 {
     [SerializeField] private GameObject _blockedCells, _virus;
+    [SerializeField] private Player _player;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _virus.SetActive(true);
+        //if (_player.GetCurrentMode() != GameMode.NormalMode) return;
+ 
+            _virus.SetActive(true);
         _blockedCells.SetActive(true);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+       // if (_player.GetCurrentMode() != GameMode.NormalMode) return;
         gameObject.SetActive(false);
     }
 }
